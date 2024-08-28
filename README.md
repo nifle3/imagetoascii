7f88d2f77d4cf686504fe6e6829067c1878b4e84
nifle3 <kuper116rus.2005@gmail.com>
2024-08-28 04:28:47 +0300

    add: init projet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imagetoascii
+
+For make testimage create a testimate folder and put there an image with name test1.jpg
---
+++ src/imagetoascii/main.py
@@ -0,0 +1,9 @@
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    pass
+
+
+if __name__ == "__main__":
+    main()
