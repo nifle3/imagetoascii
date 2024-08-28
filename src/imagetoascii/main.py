fb1b153ddb045a2beb5607382c3df51c2ad11735
nifle3 <kuper116rus.2005@gmail.com>
2024-08-28 05:12:26 +0300

    add: path validation

--- src/imagetoascii/main.py
+++ src/imagetoascii/main.py
@@ -1,8 +1,28 @@
 from typing import NoReturn
+from os import path
+import sys
 
+from PIL import Image
+
+def print_error(*args):
+    print('\033[91mERROR: ', *args, '\033[39m')
 
 def main() -> NoReturn:
-    pass
+    args = sys.argv
+    if len(args) < 2:
+        print_error('Enter a path to image')
+        sys.exit(1)
+
+    file_path = args[1]
+    if not path.exists(file_path):
+        print_error('Enter a valid file path')
+        sys.exit(1)
+
+    with Image.open(file_path)as image:
+        print(image.format, image.size, image.mode)
+        image.show()
+
+        
 
 
 if __name__ == "__main__":
